c97e0984a20eebf4ee1aa3235525ab4f7462c263
Daniel7Moldova <danielbet93@gmail.com>
2023-10-11 16:45:36 +0200

    v.0.0.1

--- FUGTSite/FUGTSite/urls.py
+++ FUGTSite/FUGTSite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from FUGTApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('acceuil/', views.acceuil, name='acceuil'),
 ]
