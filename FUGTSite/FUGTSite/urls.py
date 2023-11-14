8cd573291d291e49b569ee5303ddbc97140e5d43
Yasmine-ACHIR <achir.yasmine2020@gmail.com>
2023-11-15 00:52:21 +0100

    Set up github actions for react project

--- FUGTSite/FUGTSite/urls.py
+++ FUGTSite/FUGTSite/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-router = DefaultRouter()
 from FUGTApp.views import HelloWorldView
 from FUGTApp import views
 
-
+router = DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
