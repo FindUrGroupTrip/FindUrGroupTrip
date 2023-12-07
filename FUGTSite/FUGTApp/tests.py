f62732c8c4675e3a39ccab75891a7b2db9eaf12d
Daniel7Moldova <125097212+Daniel7Moldova@users.noreply.github.com>
2023-12-07 09:17:06 +0100

    Update tests.py

--- FUGTSite/FUGTApp/tests.py
+++ FUGTSite/FUGTApp/tests.py
@@ -5,22 +5,5 @@ from django.test import RequestFactory
 from django.http import FileResponse, HttpResponse
 from django.conf import settings
 import os
-from FUGTApp.views import serve_static_image
 
-class ServeStaticImageTest(TestCase):
-    def setUp(self):
-        self.factory = RequestFactory()
-
-    def test_serve_static_image(self):
-        image_path = os.path.join(settings.STATIC_ROOT, 'carte-interactive.jpeg')
-
-        if os.path.exists(image_path):
-            with open(image_path, 'rb') as image_file:
-                response = FileResponse(image_file)
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.get('Content-Type'), 'image/jpeg')
-        else:
-            # Le fichier n'existe pas, on s'attend à une réponse 404
-            response = serve_static_image(self.factory.get('/static/carte-interactive.jpeg/'))
-            self.assertEqual(response.status_code, 404)
 
