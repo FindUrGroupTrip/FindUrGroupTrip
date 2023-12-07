40b537fd897d90fc3ab58a88f90d2dbede12563d
Daniel7Moldova <125097212+Daniel7Moldova@users.noreply.github.com>
2023-12-07 09:05:45 +0100

    Update settings.py

--- FUGTSite/FUGTSite/settings.py
+++ FUGTSite/FUGTSite/settings.py
@@ -134,14 +134,11 @@ WSGI_APPLICATION = 'FUGTSite.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'localfugt',
-        'USER': 'root',
-        'PASSWORD': 'pass',
-        'HOST': '127.0.0.1',
-        'PORT': '3308',
-    },
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / "db.sqlite3",
+        }
+    }
     'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
