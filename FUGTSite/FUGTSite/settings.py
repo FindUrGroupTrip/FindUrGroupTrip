e417a4a763f82c555f34550aab9b258e9922d106
Daniel7Moldova <125097212+Daniel7Moldova@users.noreply.github.com>
2023-12-07 09:10:08 +0100

    Update settings.py

--- FUGTSite/FUGTSite/settings.py
+++ FUGTSite/FUGTSite/settings.py
@@ -137,12 +137,10 @@ DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / "db.sqlite3",
-        }
-    }
+    },
     'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
 }
 
 # Password validation
