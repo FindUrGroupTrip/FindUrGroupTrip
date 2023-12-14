22c0fa314bb03c8728247a122ba5d24185133f82
Yasmine-ACHIR <achir.yasmine2020@gmail.com>
2023-12-14 13:45:41 +0100

    fix to build

--- fugtreact/src/App.js
+++ fugtreact/src/App.js
@@ -1,55 +1,52 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Header } from './pages/Header';
-import { Activitelist } from './pages/activitelist';
-import ActiviteList from './pages/activitelist'; // Import your ActiviteListOLD component
-import CreerActivite from './Template/CreerActivite';
-import LeftSidebar from './LeftSideBar';
-import SingleActivite from "./Template/SingleActivite";
-import ReservationPage from './Template/reservation/ReservationPage';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Header } from './pages/Header'
+import { Activitelist } from './pages/activitelist'
+import CreerActivite from './Template/CreerActivite'
+import LeftSidebar from './LeftSideBar'
+import SingleActivite from './Template/SingleActivite'
+import ReservationPage from './Template/reservation/ReservationPage'
 import './App.scss'
-import { Sidebar } from './layout/Sidebar'
 import Login from './auth/Login'
-import Logout from './auth/Logout'
 import { Register } from './auth/Register'
 import { ProtectedRoute } from './auth/ProtectedRoute'
 import './auth/axios-interceptor'
-import HomePage from './pages/HomePage'
-import { AuthProvider } from './auth/auth-context'
 import ImageComponent from './Template/ImageComponent'
-import ReservationForm from "./Template/reservation/ReservationForm";
 
 function App() {
-    return (
-        <Router>
-            <div className="antialiased bg-gray-50 dark:bg-gray-900">
-                <Header />
-                <LeftSidebar />
-                <main className="p-4 md:ml-64 h-auto pt-20">
-                    <Routes>
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<Register />} />
-                        <Route
-                            path="/"
-                            element={
-                                <ProtectedRoute>
-                                    <CreerActivite />
-                                </ProtectedRoute>
-                            }
-                        />
-                        <Route path="/image" element={<ImageComponent />} />
-                        <Route path="/activitelist" element={<Activitelist />} />
-                        <Route path="/activites/:idactivite" element={<SingleActivite />} />
-                        <Route path="/creer-activite" element={<CreerActivite />} />
-                        <Route path="/reservation-form/:idactivite" element={<ReservationPage />} />
+  return (
+    <Router>
+      <div className="antialiased bg-gray-50 dark:bg-gray-900">
+        <Header />
+        <LeftSidebar />
+        <main className="p-4 md:ml-64 h-auto pt-20">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <CreerActivite />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/image" element={<ImageComponent />} />
+            <Route path="/activitelist" element={<Activitelist />} />
+            <Route path="/activites/:idactivite" element={<SingleActivite />} />
+            <Route path="/creer-activite" element={<CreerActivite />} />
+            <Route
+              path="/reservation-form/:idactivite"
+              element={<ReservationPage />}
+            />
 
-                        {/* Add more routes as needed */}
-                    </Routes>
-                </main>
-            </div>
-        </Router>
-    );
+            {/* Add more routes as needed */}
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
