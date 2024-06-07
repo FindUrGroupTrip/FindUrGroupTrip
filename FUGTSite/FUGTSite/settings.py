7f3d58c9ecc2918b56921480bef020de9eaa4ee2
Daniel7Moldova <danielbet93@gmail.com>
2024-06-07 21:38:02 +0200

    container

--- FUGTSite/FUGTSite/settings.py
+++ FUGTSite/FUGTSite/settings.py
@@ -135,16 +135,22 @@ WSGI_APPLICATION = 'FUGTSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'mysql.connector.django',
         'NAME': 'localfugt',
         'TEST': {
             'NAME': ':memory:',
         },
         'USER': 'root',
-        'PASSWORD': 'pass',
+        'PASSWORD': 'VtQYM8zD0317*',
         'HOST': '127.0.0.1',
-        'PORT': '3308',
+        'PORT': '3306',
     },
+
+    'sqlite': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
