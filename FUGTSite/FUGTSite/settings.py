8b966e36721012fb2bb04e9525362cd713388933
Thi Hoai Thu NINH <thninh@student.42.fr>
2024-06-07 21:27:23 +0200

    Update settings.py

--- FUGTSite/FUGTSite/settings.py
+++ FUGTSite/FUGTSite/settings.py
@@ -135,22 +135,16 @@ WSGI_APPLICATION = 'FUGTSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql.connector.django',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'localfugt',
         'TEST': {
             'NAME': ':memory:',
         },
         'USER': 'root',
-        'PASSWORD': 'VtQYM8zD0317*',
+        'PASSWORD': 'pass',
         'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'PORT': '3308',
     },
-
-    'sqlite': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
